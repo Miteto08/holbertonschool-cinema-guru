27d0403f0cbd6d4857d535b8f3dc86497f735301
Miteto08 <julien.lechevalier@shortways.com>
2025-05-23 16:04:42 +0200

    Task 7. Navigation - Sidebar

---
+++ frontend/src/components/navigation/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './navigation.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+
+function Header({ userUsername, setIsLoggedIn }) {
+    const logout = () => {
+        localStorage.removeItem('accessToken');
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <nav className="header-nav">
+            <span>Cinema Guru</span>
+            <div>
+                <img src="https://picsum.photos/100/100" alt="User Avatar" className="avatar" />
+                <p>Welcome, {userUsername}!</p>
+                <span className="logout" onClick={logout}>
+                    <FontAwesomeIcon icon={faArrowRightFromBracket} />
+                    <i className="fas fa-sign-out-alt"></i>
+                    Logout
+                </span>
+            </div>
+        </nav>
+    );
+}
+
+Header.propTypes = {
+    userUsername: PropTypes.string.isRequired,
+    setIsLoggedIn: PropTypes.func.isRequired
+};
+
+export default Header;
